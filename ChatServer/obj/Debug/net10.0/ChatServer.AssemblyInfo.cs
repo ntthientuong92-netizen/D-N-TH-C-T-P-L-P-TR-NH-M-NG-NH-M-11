f863394fd0d4bad9381aabd44bdf9c8af6268a1c
ntthientuong92-netizen <ntthientuong9.2@gmail.com>
2026-07-29 19:17:56 +0700

    Mo ta ngan gon noi dung ban vua sua

--- ChatServer/obj/Debug/net10.0/ChatServer.AssemblyInfo.cs
+++ ChatServer/obj/Debug/net10.0/ChatServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91fb18285752b28d8cfef194b97a83f70ab615c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85dca8ada2cfdea4af5a7ecb84f447af2ced38b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
