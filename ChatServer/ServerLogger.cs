b208caafbe8a471776d8c27cad20aa60cc5c0893
tuonzy-0806 <tuonvyythcs@gmail.com>
2026-09-07 00:54:49 +0700

    Cap nhat ExceptionManager va ServerLogger

--- ChatServer/ServerLogger.cs
+++ ChatServer/ServerLogger.cs
@@ -1,147 +1,29 @@
 using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Sockets;
-using System.Threading;
-using SharedLibrary;
+using System.IO;
 
 namespace ChatServer
 {
-    public class ServerCore
+    public static class ServerLogger
     {
-        private TcpListener listener;
-        private bool isRunning;
-        private Dictionary<string, TcpClient> connectedClients;
-        private int port;
-
-        public ServerCore(int port)
+        private static readonly string logFilePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "server_log.txt");
+        private static readonly object logLock = new object();
+        public static void Log(string message)
         {
-            this.port = port;
-            connectedClients = new Dictionary<string, TcpClient>();
-        }
+            string logEntry = $"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}";
 
-        public void Start()
-        {
-            try
-            {
-                listener = new TcpListener(IPAddress.Any, port);
-                listener.Start();
-                isRunning = true;
-                ServerLogger.Log($"Server đã khởi động thành công trên cổng {port}.");
+            Console.WriteLine(logEntry);
 
-                // Vòng lặp chấp nhận client kết nối
-                Thread acceptThread = new Thread(AcceptClients);
-                acceptThread.IsBackground = true;
-                acceptThread.Start();
-            }
-            catch (Exception ex)
-            {
-                ExceptionManager.HandleException(ex, "ServerCore.Start");
-            }
-        }
-
-        private void AcceptClients()
-        {
-            while (isRunning)
+            lock (logLock)
             {
                 try
                 {
-                    TcpClient client = listener.AcceptTcpClient();
-                    Thread clientThread = new Thread(() => HandleClient(client));
-                    clientThread.IsBackground = true;
-                    clientThread.Start();
+                    File.AppendAllText(logFilePath, logEntry + Environment.NewLine);
                 }
-                catch (Exception ex)
+                catch
                 {
-                    if (isRunning)
-                        ExceptionManager.HandleException(ex, "ServerCore.AcceptClients");
+
                 }
             }
-        }
-
-        private void HandleClient(TcpClient client)
-        {
-            NetworkStream stream = null;
-            string clientName = string.Empty;
-
-            try
-            {
-                stream = client.GetStream();
-                while (isRunning)
-                {
-                    MessagePacket packet = NetworkProtocol.ReceivePacket(stream);
-                    if (packet == null) break;
-
-                    if (packet.Type == PacketType.Login)
-                    {
-                        clientName = packet.Sender;
-                        lock (connectedClients)
-                        {
-                            if (!connectedClients.ContainsKey(clientName))
-                                connectedClients.Add(clientName, client);
-                            else
-                                connectedClients[clientName] = client;
-                        }
-                        ServerLogger.Log($"Client đăng nhập thành công: {clientName}");
-                    }
-
-                    // Chuyển tiếp gói tin đến các client khác (Broadcast)
-                    BroadcastPacket(packet, clientName);
-                }
-            }
-            catch (Exception ex)
-            {
-                ExceptionManager.HandleException(ex, $"HandleClient [{clientName}]");
-            }
-            finally
-            {
-                if (!string.IsNullOrEmpty(clientName))
-                {
-                    lock (connectedClients)
-                    {
-                        if (connectedClients.ContainsKey(clientName))
-                            connectedClients.Remove(clientName);
-                    }
-                    ServerLogger.Log($"Client đã ngắt kết nối: {clientName}");
-                }
-                client.Close();
-            }
-        }
-
-        private void BroadcastPacket(MessagePacket packet, string senderName)
-        {
-            byte[] dataBytes = System.Text.Json.JsonSerializer.SerializeToUtf8Bytes(packet);
-            
-            lock (connectedClients)
-            {
-                foreach (var kvp in connectedClients)
-                {
-                    // Không gửi lại cho chính người gửi
-                    if (kvp.Key != senderName)
-                    {
-                        try
-                        {
-                            NetworkStream stream = kvp.Value.GetStream();
-                            // Gửi length-prefix và byte data
-                            byte[] lengthBytes = BitConverter.GetBytes(dataBytes.Length);
-                            stream.Write(lengthBytes, 0, lengthBytes.Length);
-                            stream.Write(dataBytes, 0, dataBytes.Length);
-                            stream.Flush();
-                        }
-                        catch
-                        {
-                            // Lỗi khi gửi đến một client cụ thể sẽ được bỏ qua để không ảnh hưởng client khác
-                        }
-                    }
-                }
-            }
-        }
-
-        public void Stop()
-        {
-            isRunning = false;
-            listener?.Stop();
-            ServerLogger.Log("Server đã dừng hoạt động.");
         }
     }
 }
